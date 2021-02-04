04f2c98f6318add159001e3878718fc64872dd49
Antonkhl <54796806+Antonkhl@users.noreply.github.com>
2021-02-04 10:55:20 +0100

    BMI caculator start

---
+++ infoStrededyPattern/infoStrededyPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using infoStrededyPattern.Strategy;
+using System;
+
+
+namespace infoStrededyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the BMI caculator! Start by disclosing your gender (Male/Female)");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Whats your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Whats your height? (m)");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Whats your Weight? (kg)");
+            int weight = Convert.ToInt32(Console.ReadLine());
+     
+
+
+        }
+    }
+}
---
+++ infoStrededyPattern/infoStrededyPattern/Strategy/FormulaHeight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace infoStrededyPattern.Strategy
+{
+   namespace infoStrededyPattern
+    {
+       class FormulaHeight
+        {
+            public void HeightCaculations(double height)
+            {
+               
+            }
+        }
+    }
+}
